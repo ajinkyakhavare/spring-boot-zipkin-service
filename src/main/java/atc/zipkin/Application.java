94466b1f7c6a97b6b400589e57e1df9efc965374
Darryl Anderson <6097929+darrylanderson@users.noreply.github.com>
2018-02-20 12:36:45 -0600

    Initial import of zipkin server

---
+++ src/main/java/atc/zipkin/Application.java
@@ -0,0 +1,21 @@
+package atc.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.EnableZipkinServer;
+
+
+/**
+ * Start the Zipkin distributed tracing service.
+ */
+@SpringBootApplication
+@EnableZipkinServer
+@EnableDiscoveryClient
+public class Application
+{
+    public static void main( final String[] args )
+    {
+        SpringApplication.run( Application.class, args );
+    }
+}
